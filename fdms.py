d1ec29407e68488ffa7e981b4f7011f3b2960122
Tatiana Millan <tatimilpo@gmail.com>
2019-12-15 17:14:39 +0100

    Fixing unfeasible

--- fdms.py
+++ fdms.py
@@ -1,6 +1,9 @@
 from simulate_dual import SimulateDual
 from lcm import get_hyperperiod
 
+import numpy as np
+
+
 class Fdms:
 
     def __init__(self, tasks, show_data):
@@ -22,11 +25,13 @@ class Fdms:
         stop_point = get_hyperperiod(self.tasks)
         tasks_simulated = SimulateDual(self.tasks, stop_point, False, show_data=self.show_data)
         result, t_id = tasks_simulated.simulate()
-        while not result:
+        is_not_feasible = False
+        while not is_not_feasible and not result:
             for task in self.tasks:
                 if task.get_id() == t_id:
                     if task.get_s() == 0:
                         print("S = 0 for Task: {} UNFEASIBLE".format(t_id))
+                        is_not_feasible = True
                         break
                     else:
                         task.set_s(task.get_s() - 1)
